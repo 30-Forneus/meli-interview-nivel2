a06a4bf7b560149f56ab30292ef6cdb49b675db9
Roberto Capone <roberto.capone@enerminds.com>
2020-06-22 12:21:00 -0300

    [STEP 02] API REST detector de Mutante

---
+++ src/main/java/com/meli/api/Nivel2Application.java
@@ -0,0 +1,13 @@
+package com.meli.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nivel2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nivel2Application.class, args);
+	}
+
+}
